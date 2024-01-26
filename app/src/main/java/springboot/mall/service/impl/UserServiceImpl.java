a4023127345c972690b3a719f967d77db4f090de
zhengyi <zhengyi@cht.com.tw>
2024-01-26 10:18:19 +0800

    帳號功能 - 使用 MD5 加密密碼

--- app/src/main/java/springboot/mall/service/impl/UserServiceImpl.java
+++ app/src/main/java/springboot/mall/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
 
 import springboot.mall.dao.UserDao;
@@ -35,6 +36,11 @@ public class UserServiceImpl implements UserService{
             log.warn("Email {} has been registered.", userRegisterRequest.getEmail()); // {}內容為參數
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);  //Email已存在，回400
         }
+
+        // 使用 MD5 生成密碼的雜湊值
+        String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashedPassword);
+
         // 創建帳號
         return userDao.createUser(userRegisterRequest);
     }
@@ -43,12 +49,17 @@ public class UserServiceImpl implements UserService{
 
         User user = userDao.getUserByEmail(userLoginRequest.getEmail());
 
+        // 檢查 user是否存在
         if (user == null){
             log.warn("Email {} has not been registered.", userLoginRequest.getEmail()); // {}內容為參數
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);  //Email已存在，回400
         }
         
-        if (user.getPassword().equals(userLoginRequest.getPassword())){
+        // 使用 MD5 生成密碼的雜湊值
+        String hashedPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+
+        // 比較密碼
+        if (user.getPassword().equals(hashedPassword)){
             return user;
         }else{
             log.warn("Email {} password is not correct.", userLoginRequest.getEmail()); // {}內容為參數
